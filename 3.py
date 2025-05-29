9a59d1bd76c6a6a2c1a7af87743e42679ddb356c
motylek25 <mattew.zakharov.03@mail.ru>
2025-05-29 19:53:54 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,10 @@
+def A_40():
+    f = [1, 3]  
+    A = [1, 3]  
+    while len(A) < 40:
+        next_val = 5 * f[-1] + f[-2]
+        f.append(next_val)
+        if next_val % 2 != 0:
+            A.append(next_val)   
+    return A[39]  
+print(A_40())
